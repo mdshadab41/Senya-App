a34d3c07cb0d4f1a21d855aaeb256b01bcb7188f
mdshadab41 <shadabjhu00447@gmail.com>
2023-12-02 14:54:04 +0530

    added Attraction.kt model based off the raw JSON file of data. Added view_holder_attraction.xml for use in the UI

--- app/src/main/java/com/example/senyaapp/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/senyaapp/ui/fragment/HomeFragment.kt
@@ -13,7 +13,7 @@ class HomeFragment : BaseFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
