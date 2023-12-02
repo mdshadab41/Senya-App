fdbe318101853627771f9f3a94188b4b8126d0d0
mdshadab41 <shadabjhu00447@gmail.com>
2023-12-02 13:34:24 +0530

    Project creation and setup navgraph action and arguments

---
+++ app/src/main/java/com/example/senyaapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.senyaapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.senyaapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/senyaapp/ui/fragment/AttractionDetailFragment.kt
@@ -0,0 +1,32 @@
+package com.example.senyaapp.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.senyaapp.R
+import com.example.senyaapp.databinding.FragmentAttractionDetailBinding
+
+
+class AttractionDetailFragment : Fragment() {
+    private var _binding: FragmentAttractionDetailBinding? = null
+    private val binding = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentAttractionDetailBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
